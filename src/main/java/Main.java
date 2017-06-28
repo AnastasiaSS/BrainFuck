2e09c6c4cd0626ef2ab4ef1acf29eb129d2b3731
Anastasia Sokolan <sokolan.nastya5@yandex.ru>
2017-06-29 01:18:46 +0300

    change structure of file storage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,8 +12,6 @@ public class Main {
     public static void main(String[] args){
         Interpreter interpreter=Interpreter.getInterpreter();
         interpreter.setContext(new Context("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."));
-        //resources.setContext(new Context(",>++++++[<-------->-],>++++++[<-------->-],>++++++[<-------->-]<<[>>+>>+<<<<-]>[>+>>-<<<-]<<[>>+<<-]>>[>]>>>>++++++[<<<++++++++>>>-]<<<."));
-        //resources.setContext(new Context("-[--->+<]>-.[---->+++++<]>-.+.++++++++++.+[---->+<]>+++.-[--->++<]>-.++++++++++.+[---->+<]>+++.[-->+++++++<]>.++.-------------.[--->+<]>---..+++++.-[---->+<]>++.+[->+++<]>.++++++++++++..---.[-->+<]>--------."));
         long time=new Date().getTime();
         interpreter.interpret();
         System.out.println();
